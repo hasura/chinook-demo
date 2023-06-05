6da0be4d0e910524ba6144b58588acce656c1ad7
Chris Toth <chris.toth@hasura.io>
2023-06-05 15:46:21 +0300

    add initial data for mysql

---
+++ mysql/data-init/create-dbs.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE hge_metadata_instant;
+CREATE DATABASE hge_metadata_main;
+CREATE DATABASE hge_metadata_hr;
+CREATE DATABASE hge_metadata_crm;
+CREATE DATABASE hge_metadata_orders;
+CREATE DATABASE hge_metadata_devices;
+
+CREATE DATABASE chinook_corp;
+CREATE DATABASE chinook_music;
+CREATE DATABASE chinook_playlist;
